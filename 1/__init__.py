0308e052ad17d24e582a369f8870e1879c00871c
JL-Saludes <johnlorenzsaludes@gmail.com>
2025-11-21 01:27:23 +0000

    HELLO

--- 1/__init__.py
+++ 1/__init__.py
@@ -1,44 +1,42 @@
 import check50
-import check50.c
+import check50.java
 import re
 
 
 @check50.check()
 def exists():
-    """hello.c exists"""
-    check50.exists("hello.c")
+    """hello.java exists"""
+    check50.exists("hello.java")
 
 
 @check50.check(exists)
 def compiles():
-    """hello.c compiles"""
-    check50.c.compile("hello.c", lcs50=True)
+    """hello.java compiles"""
+    check50.java.compile("hello.java")
 
 
 @check50.check(compiles)
-def mario():
+def john():
     """responds to name John"""
     check_name("John")
 
 
 @check50.check(compiles)
-def peach():
+def lorenz():
     """responds to name Lorenz"""
     check_name("Lorenz")
 
 
 @check50.check(compiles)
-def bowser():
+def saludes():
     """responds to name Saludes"""
     check_name("Saludes")
 
 
 def check_name(name):
-    # Define expected, actual outputs
     expected = f"hello, {name}\n"
-    actual = check50.run("./hello").stdin(name).stdout()
+    actual = check50.run("java hello").stdin(name).stdout()
 
-    # Check output
     if not re.match(regex(name), actual):
         try:
             last_character = actual[-1]
@@ -49,10 +47,11 @@ def check_name(name):
             raise check50.Mismatch(
                 expected=expected,
                 actual=actual,
-                help=r"Forgot to print a newline at the end of your output?",
+                help="Forgot to print a newline at the end of your output?",
             )
         raise check50.Mismatch(expected=expected, actual=actual)
 
 
 def regex(string):
-    return f"^[Hh]ello, {re.escape(string)}\n$" 
+    # Fix: comment uses Python syntax
+    return f"^[Hh]ello, {re.escape(string)}\n$"
